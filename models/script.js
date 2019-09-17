d18f6becdf6ee561c5177a9419dc2fbc3278bcc8
Pranay Prakash <pranay.gp@gmail.com>
2019-09-17 18:52:21 -0500

    add ip to script model

--- models/script.js
+++ models/script.js
@@ -1,17 +1,18 @@
-const { Schema, SchemaTypes } = require('mongoose')
-const timestamps = require('mongoose-timestamp')
+const { Schema, SchemaTypes } = require("mongoose");
+const timestamps = require("mongoose-timestamp");
 
 const ScriptSchema = new Schema({
   key: { type: String, required: true, unique: true },
+  ip: { type: String, required: false, unique: true, sparse: true },
   url: { type: String, required: true, unique: true },
-  owner: { type: SchemaTypes.ObjectId, ref: 'Team' }
-})
+  owner: { type: SchemaTypes.ObjectId, ref: "Team" }
+});
 
 ScriptSchema.methods.canBeAccessedBy = function(team) {
-  if(!team) return false
-  if(team.admin) return true
-  return this.owner === team._id || this.owner._id === team._id
-}
+  if (!team) return false;
+  if (team.admin) return true;
+  return this.owner === team._id || this.owner._id === team._id;
+};
 
-ScriptSchema.plugin(timestamps)
-module.exports = m => m.model('Script', ScriptSchema)
+ScriptSchema.plugin(timestamps);
+module.exports = m => m.model("Script", ScriptSchema);
